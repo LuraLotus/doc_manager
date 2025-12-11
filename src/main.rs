95b8c7cb4497241454e2b7f96567694c40533eee
LuraLotus <eannlao@gmail.com>
2025-12-11 16:31:53 +0800

    Added ability to save config - Added serde to config functionality. - Added a ThemeWrapper for serde derive.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod db;
 mod screen;
 
+use std::fs;
+use std::path::Path;
+
 use iced::alignment::Horizontal::Left;
 use iced::{Element, Subscription, Task, Theme};
 use iced::widget::{Container, container, row};
@@ -9,6 +12,7 @@ use iced_aw::widget::Sidebar;
 use screen::main_menu::main_menu;
 use screen::document_list::document_list;
 use screen::settings::settings;
+use serde::{Deserialize, Serialize};
 
 use crate::screen::MainMenu;
 use crate::screen::DocumentList;
@@ -33,33 +37,184 @@ enum Message {
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Default, Copy)]
-    pub(crate) enum Tab {
-        #[default]
-        Home,
-        DocumentList,
-        Settings
+pub(crate) enum Tab {
+    #[default]
+    Home,
+    DocumentList,
+    Settings
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+enum LocalTheme {
+    Light,
+    Dark,
+    Dracula,
+    Nord,
+    SolarizedLight,
+    SolarizedDark,
+    GruvboxLight,
+    GruvboxDark,
+    CatppuccinLatte,
+    CatppuccinFrappe,
+    CatppuccinMacchiato,
+    CatppuccinMocha,
+    TokyoNight,
+    TokyoNightStorm,
+    TokyoNightLight,
+    KanagawaWave,
+    KanagawaDragon,
+    KanagawaLotus,
+    Moonfly,
+    Nightfly,
+    Oxocarbon,
+    Ferra,
+    Unknown
+}
+
+impl From<Theme> for LocalTheme {
+    fn from(theme: Theme) -> Self {
+        match theme {
+            Theme::Light => LocalTheme::Light,
+            Theme::Dark => LocalTheme::Dark,
+            Theme::Dracula => LocalTheme::Dracula,
+            Theme::Nord => LocalTheme::Nord,
+            Theme::SolarizedLight => LocalTheme::SolarizedLight,
+            Theme::SolarizedDark => LocalTheme::SolarizedDark,
+            Theme::GruvboxLight => LocalTheme::GruvboxLight,
+            Theme::GruvboxDark => LocalTheme::GruvboxDark,
+            Theme::CatppuccinLatte => LocalTheme::CatppuccinLatte,
+            Theme::CatppuccinFrappe => LocalTheme::CatppuccinFrappe,
+            Theme::CatppuccinMacchiato => LocalTheme::CatppuccinMacchiato,
+            Theme::CatppuccinMocha => LocalTheme::CatppuccinMocha,
+            Theme::TokyoNight => LocalTheme::TokyoNight,
+            Theme::TokyoNightStorm => LocalTheme::TokyoNightStorm,
+            Theme::TokyoNightLight => LocalTheme::TokyoNightLight,
+            Theme::KanagawaWave => LocalTheme::KanagawaWave,
+            Theme::KanagawaDragon => LocalTheme::KanagawaDragon,
+            Theme::KanagawaLotus => LocalTheme::KanagawaLotus,
+            Theme::Moonfly => LocalTheme::Moonfly,
+            Theme::Nightfly => LocalTheme::Nightfly,
+            Theme::Oxocarbon => LocalTheme::Oxocarbon,
+            Theme::Ferra => LocalTheme::Ferra,
+            Theme::Custom(custom) => LocalTheme::Unknown,
+        }
     }
+}
+
+impl Into<Theme> for LocalTheme {
+    fn into(self) -> Theme {
+        match self {
+            LocalTheme::Light => Theme::Light,
+            LocalTheme::Dark => Theme::Dark,
+            LocalTheme::Dracula => Theme::Dracula,
+            LocalTheme::Nord => Theme::Nord,
+            LocalTheme::SolarizedLight => Theme::SolarizedLight,
+            LocalTheme::SolarizedDark => Theme::SolarizedDark,
+            LocalTheme::GruvboxLight => Theme::GruvboxLight,
+            LocalTheme::GruvboxDark => Theme::GruvboxDark,
+            LocalTheme::CatppuccinLatte => Theme::CatppuccinLatte,
+            LocalTheme::CatppuccinFrappe => Theme::CatppuccinFrappe,
+            LocalTheme::CatppuccinMacchiato => Theme::CatppuccinMacchiato,
+            LocalTheme::CatppuccinMocha => Theme::CatppuccinMocha,
+            LocalTheme::TokyoNight => Theme::TokyoNight,
+            LocalTheme::TokyoNightStorm => Theme::TokyoNightStorm,
+            LocalTheme::TokyoNightLight => Theme::TokyoNightLight,
+            LocalTheme::KanagawaWave => Theme::KanagawaWave,
+            LocalTheme::KanagawaDragon => Theme::KanagawaDragon,
+            LocalTheme::KanagawaLotus => Theme::KanagawaLotus,
+            LocalTheme::Moonfly => Theme::Moonfly,
+            LocalTheme::Nightfly => Theme::Nightfly,
+            LocalTheme::Oxocarbon => Theme::Oxocarbon,
+            LocalTheme::Ferra => Theme::Ferra,
+            _ => Theme::Dark
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    current_theme: LocalTheme
+}
+
+impl Config {
+    fn new() -> Config {
+        let toml = fs::read_to_string("./config.toml").unwrap_or_else(|err| {
+            println!("Error reading config file: {}", err);
+            String::new()
+        });
+        match toml::from_str::<Config>(&toml) {
+            Ok(config) => config,
+            Err(err) => {
+                println!("Error deserializing config file: {}", err);
+                Config::default()
+            },
+        }
+    }
+
+    fn change_theme(&mut self, theme: Theme) {
+        self.current_theme = LocalTheme::from(theme);
+    }
+
+    fn current_theme(&self) -> LocalTheme {
+        return self.current_theme.clone()
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            current_theme: LocalTheme::from(Theme::CatppuccinMacchiato)
+        }
+    }
+}
 
 struct State {
     current_tab: Tab,
     main_menu: MainMenu,
     document_list: DocumentList,
     settings: Settings,
-    current_theme: Theme,
+    config: Config,
     previous_tab: Option<Tab>
 }
 
 impl State {
     fn new() -> State {
-        let initial_theme = Theme::CatppuccinMacchiato;
+        let config = Config::new();
+        let initial_theme = config.current_theme();
         let mut settings = Settings::new();
-        settings.set_theme(initial_theme.clone());
+        settings.set_theme(
+            match initial_theme {
+                LocalTheme::Light => Theme::Light,
+                LocalTheme::Dark => Theme::Dark,
+                LocalTheme::Dracula => Theme::Dracula,
+                LocalTheme::Nord => Theme::Nord,
+                LocalTheme::SolarizedLight => Theme::SolarizedLight,
+                LocalTheme::SolarizedDark => Theme::SolarizedDark,
+                LocalTheme::GruvboxLight => Theme::GruvboxLight,
+                LocalTheme::GruvboxDark => Theme::GruvboxDark,
+                LocalTheme::CatppuccinLatte => Theme::CatppuccinLatte,
+                LocalTheme::CatppuccinFrappe => Theme::CatppuccinFrappe,
+                LocalTheme::CatppuccinMacchiato => Theme::CatppuccinMacchiato,
+                LocalTheme::CatppuccinMocha => Theme::CatppuccinMocha,
+                LocalTheme::TokyoNight => Theme::TokyoNight,
+                LocalTheme::TokyoNightStorm => Theme::TokyoNightStorm,
+                LocalTheme::TokyoNightLight => Theme::TokyoNightLight,
+                LocalTheme::KanagawaWave => Theme::KanagawaWave,
+                LocalTheme::KanagawaDragon => Theme::KanagawaDragon,
+                LocalTheme::KanagawaLotus => Theme::KanagawaLotus,
+                LocalTheme::Moonfly => Theme::Moonfly,
+                LocalTheme::Nightfly => Theme::Nightfly,
+                LocalTheme::Oxocarbon => Theme::Oxocarbon,
+                LocalTheme::Ferra => Theme::Ferra,
+                _ => Theme::Dark
+            }
+        );
         State {
             current_tab: Tab::default(),
             main_menu: MainMenu::new(),
             document_list: DocumentList::new(),
             settings,
-            current_theme: initial_theme,
+            config,
             previous_tab: None
         }
     }
@@ -112,7 +267,14 @@ impl State {
             Message::Settings(settings_message) => {
                 match settings_message {
                     settings::Message::ChangeTheme(theme) => {
-                        self.current_theme = theme.clone();
+                        self.config.change_theme(theme.clone());
+                        let serialized = toml::to_string(&self.config).unwrap_or_else(|err| {
+                            println!("Error serializing config to toml: {}", err);
+                            String::new()
+                        });
+                        fs::write("./config.toml", serialized).unwrap_or_else(|err| {
+                            println!("Error writing to config file: {}", err);
+                        });
                         self.settings.set_theme(theme);
                     }
                     settings::Message::Back => {
@@ -163,7 +325,7 @@ impl State {
     }
 
     fn current_theme(&self) -> Theme {
-        self.current_theme.clone()
+        self.config.current_theme().into()
     }
 }
 
